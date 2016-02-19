27ede82d7133b6f5ee664baec97b88232ae534ac
Toshiba Laptop <sarathm00@gmail.com>
2016-02-19 23:42:32 +0530

    Almost Working

--- skinTrack2.py
+++ skinTrack2.py
@@ -1,11 +1,80 @@
 import cv2
 import numpy as np
+import dlib
+import imutils
+from math import sqrt
+
 
 cap=cv2.VideoCapture(0)
+
+class Dlib():
+	def __init__(self):
+		self.PREDICTOR_PATH = "shape_predictor_68_face_landmarks.dat"
+		MOUTH_POINTS = list(range(48, 61))
+		self.OVERLAY_POINTS = [MOUTH_POINTS]
+
+		self.detector = dlib.get_frontal_face_detector()
+		self.predictor = dlib.shape_predictor(self.PREDICTOR_PATH)
+	
+	def get_landmarks(self,img):
+	    rects = self.detector(img, 0)
+	    
+	    if len(rects) > 1:
+	        print 'TooManyFaces'
+	    if len(rects) == 0:
+	    	raise ValueError('Error: NoFaces!!')
+	    	#print "NoFaces"
+
+	    return np.matrix([[p.x, p.y] for p in self.predictor(img, rects[0]).parts()])
+
+
+	def get_face_mask(self,img,landmarks):
+	    for group in self.OVERLAY_POINTS:
+	        hull = cv2.convexHull(landmarks[group])
+	        cv2.fillConvexPoly(img, hull, 0)
+
 
 def disp(img,string,coordinates):
 	font = cv2.FONT_HERSHEY_SIMPLEX
 	cv2.putText(img,string,coordinates, font, 1,(255,0,0),2,1)
+
+def lipSegment(img):
+	img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+	img_copy = img.copy()
+	#img = imutils.resize(img,width=300)
+
+	landmarks = dlib_obj.get_landmarks(img)
+
+	dlib_obj.get_face_mask(img_copy, landmarks)
+	output_img = img-img_copy
+	
+	#output_img = cv2.cvtColor(output_img,cv2.COLOR_BGR2GRAY)
+	contours,hierarchy = cv2.findContours(output_img.copy(), cv2.cv.CV_RETR_EXTERNAL, cv2.cv.CV_CHAIN_APPROX_SIMPLE)  #cv2.findContours(image, mode, method
+	#cv2.drawContours(img, contours, -1, (0,255,0), 2,maxLevel=0)
+	cnt = contours[0]
+	ellipse = cv2.fitEllipse(cnt)
+	(x,y),(MA,ma),angle = cv2.fitEllipse(cnt)
+	cv2.ellipse(img,ellipse,(0,255,0),2)
+
+	
+	a = ma/2
+	b = MA/2
+
+
+	eccentricity = sqrt(pow(a,2)-pow(b,2))
+	eccentricity = round(eccentricity/a,2)
+
+	font = cv2.FONT_HERSHEY_SIMPLEX
+
+	cv2.putText(img,'ma = '+str(round(ma,2)),(10,300), font, 1,(255,0,0),2,16)
+	cv2.putText(img,'MA = '+str(round(MA,2)),(10,350), font, 1,(255,0,0),2,16)
+	cv2.putText(img,'Eccentricity = '+str(round(eccentricity,3)),(10,400), font, 1,(255,0,0),2,16)
+	
+	if(eccentricity < 0.84):
+		cv2.putText(img,'Commands = O',(10,450), font, 1,(0,0,255),2,16)
+	else:
+		cv2.putText(img,'Commands = E',(10,500), font, 1,(0,0,255),2,16)
+	cv2.imshow("HEad",img)
 
 def count_fingers(cnts,hand_frame,img):
 	if cnts:
@@ -56,11 +125,17 @@ def main():
 		img = cv2.medianBlur(img,3)    # 5 is a fairly small kernel size
 		img = cv2.resize(img,None,fx=1.2,fy=1.2,interpolation = cv2.INTER_LINEAR)
 		cv2.rectangle(img,(0,200),(250,500),(50,50,50),2)
-		cv2.rectangle(img,(300,200),(600,500),(50,50,50),2)
+		cv2.rectangle(img,(300,100),(600,500),(50,50,50),2)
 		
 		hand_frame = img[200:500,0:250]
-		head_frame = img[200:500,300:600]
+		head_frame = img[100:500,300:600]
+		#head_frame = img.copy()
 
+		try:
+			lipSegment(head_frame)	
+		except ValueError, e:
+			print e
+		
 		hsv_btn1 = cv2.cvtColor(hand_frame,cv2.COLOR_BGR2HSV)
 		lower_skin = np.array([0,7,30])
 		upper_skin = np.array([150,90,120])
@@ -78,6 +153,7 @@ def main():
 		#cv2.imshow('mask',mask)
 		#cv2.imshow('Res',res)
 		cv2.imshow('Img',img)
+		cv2.imshow('head_frame',head_frame)
 		
 		if cv2.waitKey(20)&0xff==ord('q'):
 			cv2.imwrite('hand_frame.jpg', hand_frame)
@@ -88,4 +164,5 @@ def main():
 	cv2.destroyAllWindows()
 
 if __name__ == '__main__':
+	dlib_obj = Dlib()
 	main()
