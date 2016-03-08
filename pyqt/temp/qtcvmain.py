95c8b9394a24ddbf7adb3122ff43de203b0ee295
Toshiba Laptop <sarathm00@gmail.com>
2016-03-08 21:22:32 +0530

    Face Detection Working

--- pyqt/temp/qtcvmain.py
+++ pyqt/temp/qtcvmain.py
@@ -3,19 +3,58 @@ import cv2
 import numpy as np
 from PyQt4 import QtGui, QtCore, Qt
 from ui import Ui_MainWindow
+import numpy as np
+import dlib
+import imutils
+from math import sqrt
+
+
+class Dlib():
+	def __init__(self):
+		self.PREDICTOR_PATH = "/home/sm/Documents/SkinDetection/shape_predictor_68_face_landmarks.dat"
+		MOUTH_POINTS = list(range(48, 61))
+		self.OVERLAY_POINTS = [MOUTH_POINTS]
+
+		self.detector = dlib.get_frontal_face_detector()
+		self.predictor = dlib.shape_predictor(self.PREDICTOR_PATH)
+	
+	def get_landmarks(self,img):
+		rects = self.detector(img, 0)
+		
+		if len(rects) > 1:
+			print 'TooManyFaces'
+		if len(rects) == 0:
+			raise ValueError('Error: NoFaces!!')
+
+		return np.matrix([[p.x, p.y] for p in self.predictor(img, rects[0]).parts()])
+
+
+	def get_face_mask(self,img,landmarks):
+		for group in self.OVERLAY_POINTS:
+			hull = cv2.convexHull(landmarks[group])
+			cv2.fillConvexPoly(img, hull, 0)
+
 
 class Video():
 	def __init__(self,capture):
 		self.capture = capture
 		self.currentFrame=np.array([])
- 
+ 		self.dlib_obj = Dlib()
 	def captureNextFrame(self):
 		"""                           
 		capture frame and reverse RBG BGR and return opencv image                                      
 		"""
 		ret, readFrame=self.capture.read()
-		if(ret==True):
-			self.currentFrame=cv2.cvtColor(readFrame,cv2.COLOR_BGR2RGB)
+		
+		try:
+			readFrame = self.lipSegment(readFrame)
+		except ValueError, e:
+			print e
+		
+		if not ret :
+			print "Error reading Frame!!"
+			sys.exit(0)
+		self.currentFrame=cv2.cvtColor(readFrame,cv2.COLOR_BGR2RGB)
  
 	def convertFrame(self):
 		"""     converts frame to format suitable for QtGui            """
@@ -31,7 +70,41 @@ class Video():
 		except Exception,e:
 			print "convertFrame: ",e
 			return None
-			
+	def lipSegment(self,img):
+		#img = imutils.resize(img,width=300)
+		img_copy = img.copy()
+
+		landmarks = self.dlib_obj.get_landmarks(img)
+		self.dlib_obj.get_face_mask(img_copy, landmarks)
+		
+		output_img = img-img_copy
+		output_img = cv2.cvtColor(output_img,cv2.COLOR_BGR2GRAY)
+		
+		contours,hierarchy = cv2.findContours(output_img.copy(), cv2.cv.CV_RETR_EXTERNAL, cv2.cv.CV_CHAIN_APPROX_SIMPLE)  #cv2.findContours(image, mode, method
+		cv2.drawContours(img, contours, -1, (0,255,0), 2,maxLevel=0)
+		
+		cnt = contours[0]
+		ellipse = cv2.fitEllipse(cnt)
+		(x,y),(MA,ma),angle = cv2.fitEllipse(cnt)
+		
+		
+		a = ma/2
+		b = MA/2
+
+
+		eccentricity = sqrt(pow(a,2)-pow(b,2))
+		eccentricity = round(eccentricity/a,2)
+
+		font = cv2.FONT_HERSHEY_SIMPLEX
+
+		cv2.putText(img,'Eccentr= '+str(round(eccentricity,3)),(10,350), font, 1,(255,0,0),2,16)
+		
+		if(eccentricity < 0.9):
+			cv2.putText(img,'Commands = O',(10,300), font, 1,(0,0,255),2,16)
+		else:
+			cv2.putText(img,'Commands = E',(10,300), font, 1,(0,0,255),2,16)
+
+		return img			
  
 class Gui(QtGui.QMainWindow):
 	def __init__(self,parent=None):
