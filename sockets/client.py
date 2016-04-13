f3ac72c85c35f70f76b80751fd935c82fd4c6be7
Work PC <sarathm00@gmail.com>
2016-04-13 17:02:32 +0530

    Test code

--- sockets/client.py
+++ sockets/client.py
@@ -8,26 +8,31 @@ except socket.error:
     print 'Failed to create socket'
     sys.exit()
 
-host = '192.168.1.7'
+host = '192.168.1.12'
 port = 8888
 
-while True:
-    msg = raw_input('Enter message to send : ')
-    try:
-        # Set the whole string
-        s.sendto(msg, (host, port))
-        # receive data from client (data, addr)
-        d = s.recvfrom(1024)
-        reply = d[0]
-        addr = d[1]
+try:
+    while True:
+        msg = raw_input('Enter message to send : ')
+        try:
+            # Set the whole string
+            s.sendto(msg, (host, port))
+            # receive data from client (data, addr)
+            d = s.recvfrom(1024)
+            reply = d[0]
+            addr = d[1]
 
-        print 'Server reply : ' + reply
-    except socket.error, msg:
-        print "Socket Error"
-        print 'Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
-        s.close()
-        sys.exit()
-    except KeyboardInterrupt, e:
-        print "Ctr + C ==> Quiting"
-        s.close()
-        sys.exit()
+            print 'Server reply : ' + reply
+        except socket.error, msg:
+            print "Socket Error"
+            print 'Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+            s.close()
+            sys.exit()
+        except KeyboardInterrupt, e:
+            print "Ctr + C ==> Quiting"
+            s.close()
+            sys.exit()
+except KeyboardInterrupt, e:
+    print "Ctr + C ==> Quiting"
+    s.close()
+    sys.exit()
