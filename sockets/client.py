61e57fc578e8df21c7c01c4b9150e928641be97b
Work PC <sarathm00@gmail.com>
2016-04-14 14:37:02 +0530

    Added socket timeout for blocking statements

--- sockets/client.py
+++ sockets/client.py
@@ -4,10 +4,10 @@ import sys  # for exit
 # create dgram udp socket
 try:
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(2.0)
 except socket.error:
     print 'Failed to create socket'
     sys.exit()
-
 host = '192.168.1.12'
 port = 8888
 
@@ -15,17 +15,24 @@ try:
     while True:
         msg = raw_input('Enter message to send : ')
         try:
-            # Set the whole string
             s.sendto(msg, (host, port))
+            if msg == 'q':
+                print "Raising KeyboardInterrupt"
+                raise KeyboardInterrupt
+            # Set the whole string
             # receive data from client (data, addr)
             d = s.recvfrom(1024)
+
             reply = d[0]
             addr = d[1]
 
             print 'Server reply : ' + reply
+        except socket.timeout, e:
+            print "Socket timeout"
+
         except socket.error, msg:
             print "Socket Error"
-            print 'Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+            print 'Error Code : ', msg
             s.close()
             sys.exit()
         except KeyboardInterrupt, e:
