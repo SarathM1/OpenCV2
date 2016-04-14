c72ae8b6cb4185e9f4be1c9e72a14bc9395562f8
Work PC <sarathm00@gmail.com>
2016-04-14 15:47:46 +0530

    Minor change

--- sockets/server.py
+++ sockets/server.py
@@ -84,6 +84,8 @@ if __name__ == '__main__':
     obj = Server()
     try:
         obj.run()
+    except KeyboardInterrupt:
+        print "\n\tKeyboardInterrupt"
     finally:
         print "\n\tQuitting"
         obj.s.close()
